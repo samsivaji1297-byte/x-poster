e0d9507b3f95ca6299084052a9d34bffbb6ece3f
samsivaji1297-byte <samsivaji1297@gmail.com>
2026-01-13 14:38:38 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import { TwitterApi } from "twitter-api-v2";
+
+async function main() {
+  const client = new TwitterApi({
+    appKey: process.env.X_API_KEY,
+    appSecret: process.env.X_API_SECRET,
+    accessToken: process.env.X_ACCESS_TOKEN,
+    accessSecret: process.env.X_ACCESS_SECRET,
+  });
+
+  const posts = JSON.parse(fs.readFileSync("posts.json", "utf8"));
+  const nextPost = posts.find(p => p.posted === false);
+
+  if (!nextPost) {
+    console.log("No posts left.");
+    return;
+  }
+
+  try {
+    const response = await client.v2.tweet(nextPost.text);
+    console.log("Posted:", response);
+
+    nextPost.posted = true;
+    fs.writeFileSync("posts.json", JSON.stringify(posts, null, 2));
+  } catch (err) {
+    console.error("Error posting:", err);
+  }
+}
+
+main();
